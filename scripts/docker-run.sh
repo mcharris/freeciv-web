d3dae1c795fc94502b81e638a4e3a15d832007bc
mcharris <mcharris@unomaha.edu>
2017-09-13 13:01:56 -0500

    Update docker-run.sh

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -4,3 +4,5 @@ service mysql start
 service nginx start
 cd /docker/scripts/
 sudo -u freeciv ./start-freeciv-web.sh
+
+tail -f  /docker/logs/pbem.log /docker/logs/meta-stats.log /docker/logs/freeciv-earth.log
